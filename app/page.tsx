734b31a662a37e79570c201d2999e671e0a17e98
Eric Vo <vo.eric@gmail.com>
2025-06-07 12:49:53 -0400

    Update blurb

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from 'app/components/posts';
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
         My Portfolio
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+      <p className='mb-4'>
+        {`I'm a New York based software engineer with two years of experience working at pre-seed and seed-stage startups. I have a background in the hospitality industry `}
       </p>
-      <div className="my-8">
+      <div className='my-8'>
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
